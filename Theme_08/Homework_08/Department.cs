ee6c07bb3b7acf18c12b91bbfeffcb940e2cfed7
Сергей Казьмин <kazmin5796@mail.ru>
2021-05-21 13:28:49 +0300

    Добавьте файлы проекта.

---
+++ Theme_08/Homework_08/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_08
+{
+    struct Department
+    {
+        public string nameOfDepartment;
+        public DateTime dateOfCreate;
+        public int numOfWorkers;
+        public List<Department> departments;
+        public Department(string nameOfDepartment, DateTime dateOfCreate, int numOfWorkers, List<Department> departments)
+        {
+            this.nameOfDepartment = nameOfDepartment;
+            this.dateOfCreate = dateOfCreate;
+            this.numOfWorkers = numOfWorkers;
+            this.departments = departments;
+        }
+    }
+}
---
+++ Theme_08/Homework_08/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_08
+{
+    struct Worker
+    {
+        public string name;
+        public string surname;
+        public short age;
+        public Department departmentName;
+        public int ID;
+        public int salary;
+        public short numOfProjects;
+        public Worker(string name, string surname, short age, Department departmentName, int ID, int salary, short numOfProjects)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.departmentName = departmentName;
+            this.ID = ID;
+            this.salary = salary;
+            this.numOfProjects = numOfProjects;
+        }
+    }
+}
